1d685f3f731258b786433e8932c8175b80867dfe
Bhakti <jivanibhakti@gmail.com>
2016-02-11 12:28:00 -0800

    11feb

--- hw3/from.php
+++ hw3/from.php
@@ -5,185 +5,173 @@
 <title>HW3</title>
 </head>
 <body >
-<form class="chapter-08">
-<script>
-document.addEventListener('DOMContentLoaded', function () {
-   'use strict';
-   var inputField = document.querySelectorAll('input'),
-         iLen = inputField.length,i;
-   function statusMsg(e) {
-         var valid = e.currentTarget.checkValidity(),
-               validStatus = e.currentTarget.validity,
-               validMsg = e.currentTarget.nextSibling,
-               status;
-         console.log(validStatus);
-         if (valid) {
-            status = '?';
-         } else {
-            if (validStatus.patternMismatch) {
-               status = 'Pattern mismatch';
-            } else if (validStatus.typeMismatch) {
-               status = 'Type mismatch';
-            } else if (validStatus.valueMissing) {
-               status = 'Value missing';
-            } else {
-               status = 'Unknown error';
-            }
-         }
-         validMsg.textContent = status;
-   }
-   for (i = 0; i < iLen; i++) {
-      inputField[i].addEventListener('input', statusMsg, false);
-   }
-}, false);
-
-var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
-var msg;
-
-db.transaction(function (tx) {
-  tx.executeSql('CREATE TABLE IF NOT EXISTS From(firstname text not Null,lastname text not null,company text not null, log)');
+<form  id ="main" name="contact_form" >
+  <link rel="stylesheet" type="text/css" href="from.css">
+  <h4> <img src="pdf.png" height="41" width="42" /> Please fill out this from before downloading.</h4>
+  <br>
+  <div class="solidline"></div>
+  <table cellspacing="10">
+    <tr>
+      <td> FirstName:* </td>
+      <td><input type="text" name="firstName" placeholder="Enter FirstName" required></td>
+      <td> Company:*</td>
+      <td><input type="text" name="company" required></td>
+    </tr>
+    <tr>
+      <td> LastName:*</td>
+      <td><input type="text" name="lastName" placeholder="Enter LastName" required></td>
+      <td> Website:*</td>
+      <td><input name="website" id="website" type="url" placeholder="http://www.w3schools.com/" required></td>
+    </tr>
+    <tr>
+      <td> Email:* </td>
+      <td><input name="email" id="email" type="email" placeholder="xyz@gmail.com" autocomplete="off" required></td>
+      <td> Position:</td>
+      <td><select name="position" id="position">
+          </font>
+          <option value="" selected="selected">-- Select One</option>
+          <option name="Software Engineering">Software Engineering</option>
+          <option name="Graphics Designer">Graphics Designer</option>
+          <option name="full stack developer">full stack developer</option>
+          <option name="Web developer">Web developer</option>
+          <option name="internship">internship</option>
+          <option name="networking">networking</option>
+        </select></td>
+    </tr>
+    <tr>
+      <td> Password: </td>
+      <td><input type="text" name="password"></td>
+      <td> Phone:*</td>
+      <td><input type="text" name="phone" placeholder="Please enter In Number" required></td>
+    </tr>
+    <tr>
+      <td colspan = 4> *Setting a password will allow you to log back in and see our see our documents any time.</font></td>
+    </tr>
+  </table>
+  <br>
+  <div class="solidline"></div>
+  <br>
   
-  msg = '<p>Log message created and row inserted.</p>';
-  document.querySelector('#status').innerHTML =  msg;
-});
+  PRIMARY service or product of interest: *<br>
+  <br>
+  <select name="primary" id="primary" required>
+    <option value="" selected="selected">-- Select One</option>
+    <option name="Personal">Personal</option>
+    <option name="Business">Business</option>
+    <option name="Health">Health</option>
+    <option name="Science">Science</option>
+    <option name="Education">Education</option>
+    <option name="Finance">Finance</option>
+    <option name="Consumer">Consumer</option>
+  </select>
+  <br>
+  <br>
+  <br>
+  OTHER services or product of interest: <br>
+  <br>
+  <input type="checkbox" name="other1" id="other1" value="Online Strategy">
+  Online Strategy <br>
+  <br>
+  <input type="checkbox" name="other2" id="other2" value="Web Design and Devlopment">
+  Web Design and Devlopment <br>
+  <br>
+  <input type="checkbox" name="other3" id="other3" value="Socail Networking and Online Community">
+  Socail Networking and Online Community <br>
+  <br>
+  <input type="checkbox" name="other4" id="other4" value="Intergrated Online Campaigns">
+  Intergrated Online Campaigns <br>
+  <br>
+  <input type="checkbox" name="other5" id="other5" value="Email Marketing Services (Strategy, Design, Devlopment)">
+  E-mail Marketing Services (Strategy, Design, Devlopment) <br>
+  <br>
+  <input type="checkbox" name="other6" id="other6" value="Email Marketing Software">
+  E-mail Marketing Software
+  </div>
+  </font><br>
+  <br>
+  <div class="solidline"></div>
+  <br>
+  <br>
+  When are you looking to implement a solution? *<br>
+  <br>
+  <label>
+  <select name="time" id="time" required>
+    <option value="" selected="selected">-- Select One</option>
+    <option name="today">Today</option>
+    <option name="tomorrow">Tomorrow</option>
+    <option name="this week">This Week</option>
+    <option name="this month">This Month</option>
+    <option name="this year">This Year</option>
+    <option name="never">Never</option>
+  </select>
+  </label>
+  </font> <br>
+  <br>
+  <div class="solidline"></div>
+  <br>
+  <div> YES! Subscribe to the weekly news letter from our website <br>
+    <br>
+    <input type="checkbox" name="subscribe" id="subscribe" class="chkbx">
+    Receive feature and best practice articles on e-mail marketing,interactive design and online strategy. <br>
+  </div>
+  <div> <br>
+    <input id="button" type="submit" value="save" name="save" onclick="return addUser()" />
+    <br>
+  </div>
+  <br>
+  <br>
+  </div>
+  <pre>
+
+</pre>
+</form>
+<script>
+var db = openDatabase('mydb', '1.0', 'Test DB', 2 * 1024 * 1024);
 
 db.transaction(function (tx) {
-  tx.executeSql('SELECT * FROM LOGS', [], function (tx, results) {
-   var len = results.rows.length, i;
-   msg = "<p>Found rows: " + len + "</p>";
-   document.querySelector('#status').innerHTML +=  msg;
-   for (i = 0; i < len; i++){
-     msg = "<p><b>" + results.rows.item(i).log + "</b></p>";
-     document.querySelector('#status').innerHTML +=  msg;
-   }
- }, null);
-});
+   t.executeSql("CREATE TABLE IF NOT EXISTS users (firtname Text,lastname Text,email Text,Web Text,Company Text,password Text,Phone Text,position Text,position1 Text,position2 Text)");
+    });
 
+
+
+function adduser(){
+    var myform = document.getElementById("contact_form");
+    if(myform.checkValidity()){
+        if (mydb) {
+            var firstname_t = document.getElementById("Fname").value;
+            var lastname_t = document.getElementById("Lname").value;
+            var email_t = document.getElementById("Email").value;
+            var web_t = document.getElementById("Web").value;
+            var company_t = document.getElementById("Comp").value;
+            var password_t = document.getElementById("password").value;
+            var phone_t = document.getElementById("Teldata").value;
+            var position_t = document.getElementById("position").value;
+            var position1_t = document.getElementById("position1").value;
+            var otherservices = "";
+            var x = document.getElementsByName("checkbox");
+            var i;
+            for(i=0;i<x.length;i++){
+                if(x[i].checked){
+                    otherservices += x[i].value + ",";
+                }
+            }
+            otherservices = otherservices.replace(/,(?=[^,]*$)/, '');
+         var month = document.getElementById("position2").value;
+         var received = document.getElementById("subscribe").checked ? "Yes" : "No";
+
+         mydb.transaction(function (t) {
+             t.executeSl("INSERT INTO users (firtname,lastname,email,Web,Company,password,Phone,position,position1,position2) Values(?,?,?,?,?,?,?,?,?,?,?,?)"),
+             [firstname_t,lastname_t,email_t,web_t,company_t,password_t,phone_t,position_t,position1_t,]
+             alert("Save successfully.");
+             window.location.href = 'index.html';
+
+         });
+
+        }else{
+        alert("db not found,your browser does not support SQl");
+        }
+    }
+}
 </script>
-  <div>
-    <link rel="stylesheet" type="text/css" href="from.css">
-    <h4><img src="pdf.png" height="41" width="42" />Please fill out this from before downloading.</h4>
-    <hr/>
-    <table width="1093" height="165">
-      <tr>
-        <td width="281" height="28"><label for="fname">First Name:*</label></td>
-        <td width="275"><input name="text" type="text" id="fname" placeholder="First Name" spellcheck="true" autocomplete="off" autofocus />
-        </td>
-        <td width="257" height="28"><label for="cname">Company:*</label></td>
-        <td width="260"><input name="text" type="text" id="cname" placeholder="Company Name" spellcheck="true"autofocus />
-        </td>
-      </tr>
-      <tr>
-        <td height="29"><label for="Lname">Lastname:*</label></td>
-        <td><input name="lastname" type="lastname" id="Lname" placeholder="Last Name" spellcheck="true" autofocus />
-        </td>
-        <td width="257" height="28"><label for="website">Website:*</label></td>
-        <td width="260"><input name="text" type="text" id="website" placeholder="Website" spellcheck="true"  autofocus />
-        </td>
-      </tr>
-      <tr>
-        <td><label for="Email">Email:*</label></td>
-        <td><input name="email" type="email" id="Email" placeholder="Email" autofocus required />
-        </td>
-        <td width="257" height="28"><label for="position">Position:</label></td>
-        <td width="260"><select>
-            <option value="Select one">--Select One</option>
-            <option value="Web Designer">Web Designer</option>
-            <option value="Software Engineer">Software Engineer</option>
-            <option value="Web developer">Web developer</option>
-            <option value="Graphics Designer">Graphics Designer</option>
-          </select>
-        </td>
-      </tr>
-      <tr>
-        <td><label for="Password">Password:</label></td>
-        <td><input name="password" type="password" id="search" placeholder="Password" />
-        </td>
-        <td width="257" height="24"><label for="phone">Phone:*</label></td>
-        <td width="260"><input name="text" type="text" id="phone" placeholder="Phone" pattern="\d*" required autofocus />
-        </td>
-      </tr>
-      <tr>
-        <td height="42" colspan="4">Setting a password will allow you to log back in and see our documents any time.</td>
-      </tr>
-    </table>
-    <hr/>
-    <br />
-    <table>
-      <tr>
-        <td><b>PRIMARY Service or Product of Interest:*</b></td>
-      </tr>
-      <tr>
-        <td><select>
-            <option value="Select one">--Select One</option>
-            <option value="Payrolling">Payrolling</option>
-            <option value="Testing">Testing</option>
-            <option value="Contract">Contract</option>
-          </select></td>
-      </tr>
-      <tr>
-        <td><b>OTHER Service or Product of Interest:</b></td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Stretegy" value="online Strategy">
-          Online Strategy<br></td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Webdesign" value="webdesign" >
-          Web Design and Development<br></td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Socialnetworking" value="Socialnetworking" >
-          Social Networking and Online Community</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Integrated Online Campalgns" value="Integrated Online Campalgns" >
-          Integrated Online Campalgns</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Email Marketing Services" value="Email Marketing Services" >
-          Email Marketing Services(Strategy,Design,Development)</td>
-      </tr>
-      <tr>
-        <td><input type="checkbox" name="Email Marketing Software" value="Email Marketing Software">
-          Email Marketing Software<br></td>
-      </tr>
-    </table>
-    <hr/>
-    <br />
-    <table>
-      <tr>
-        <td><b>When are you looking to implement a solution?*</b></td>
-      </tr>
-      <tr>
-        <td><select>
-            <option value="Select one">--Select One</option>
-            <option value="ERP Solution">ERP Solution</option>
-            <option value="iphone">iphone</option>
-            <option value="Android">Android</option>
-          </select></td>
-      </tr>
-    </table>
-    <br />
-    <hr/>
-    <table>
-      <tr>
-        <td><b>Yes! Subscribe to</b></td>
-      </tr>
-      <tr>
-        <td><p>
-            <input type="checkbox" name="Stretegy" value="online Strategy" checked="checked">
-            Receive feature and best practice articles on email marketing,
-          <p> interactive design and online strategy.</td>
-      </tr>
-    </table>
-    <table>
-      <tr>
-        <td width="142"><button class="w3-btn w3-round-large">Save</button></td>
-      </tr>
-    </table>
-  </div>
-</form>
-
 </body>
 </html>
